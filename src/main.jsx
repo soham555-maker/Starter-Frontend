82fc6c357cf99e85f854fcc147b78efc534febc0
Soham Sunil Margaj <sohammargaj55555@gmail.com>
2024-03-18 23:34:59 +0530

    Add dependencies and routes for error handling and root component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import App from './App.jsx'
 import './index.css'
+import Root from './routes/Root/Root.jsx';
+import Error from './routes/Error/Error.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <div >Home</div>,
+        errorElement: <Error/>
+      },
+      {
+        path: "login",
+        element: <div >login</div>,
+        errorElement: <Error/>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
---
+++ src/routes/Root/Root.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import './Root.css';
+import reactLogo from '../../assets/react.svg';
+import viteLogo from '../../assets/vite.svg';
+
+export default function Root() {
+    return (
+        <div>
+        <nav>
+            <ul>
+                <li><Link className='link' to="/"><img src={reactLogo} alt="REACT" /></Link></li>
+                <li> + </li>
+                <li><Link className='link' to="/"><img src={viteLogo} alt="REACT" /></Link></li>
+
+            </ul>
+            <ul>
+                <li><Link className='link' to="/">Home</Link></li>
+            </ul>
+            <ul>
+                <li><Link className='link' to="/login">Login</Link></li>
+            </ul>
+        </nav>
+        <div className="outlet-container">
+            <Outlet />
+        </div>
+        </div>
+    )
+    }
